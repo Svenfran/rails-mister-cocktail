f97fb2dc558cfb8e09692ae551d8872a6e6e089d
Sven Haider <sven.haider@web.de>
2020-05-08 12:20:35 +0200

    edit manifest.js

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,30 @@
+class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: :show
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+  end
+  
+  def new
+    @cocktail = Cocktail.new
+  end
+  
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+  end
+  
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:id)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+  
+  
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+Cocktail.new(name: "Blue Ocean")
+Cocktail.new(name: "Batida Brazil")
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
